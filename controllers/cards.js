63db5bae34a9d0c95b3f1c84e3d4edf70da8ac89
Artem Noskov <artyom.noskov.1998@yandex.ru>
2021-02-02 22:43:52 +0500

    fix errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,26 +3,29 @@ const Card = require('../models/card');
 const getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.status(200).send(cards))
-    .catch(() => res.status(400).send({ message: 'На сервере произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
 const addCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id; // _id станет доступен
-  Card.create({ name, link, owner })
+  Card.create(owner, { name, link }, { new: true, runValidators: true })
     .then((card) => res.send({ data: card }))
     .catch(() => res.status(400).send({ message: 'На сервере произошла ошибка' }));
 };
 
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send({ data: card }))
-    .catch((err) => {
-      if (err.message === '404') {
+    .then((card) => {
+      if (!card) {
         return res.status(404).send({ message: 'Такой карточки не существует' });
       }
+
+      res.send({ data: card });
+    })
+    .catch((err) => {
       if (err instanceof mongoose.CastError) {
-        return res.status(400).send({ message: 'Нет карточки с таким id' });
+        return res.status(400).send({ message: 'Передан не валидный id' });
       }
       return res.status(500).send({ message: 'На сервере произошла ошибка' });
     });
--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
-    .catch(() => res.status(400).send({ message: 'Нет пользователя с таким id' }));
+    .catch(() => res.status(500).send({ message: 'Нет пользователя с таким id' }));
 };
 
 const getUsersId = (req, res) => {
@@ -16,29 +16,39 @@ const getUsersId = (req, res) => {
 
       return res.status(200).send(user);
     })
-    .catch(() => res.status(400).send({ message: 'Нет пользователя с таким id' }));
+    .catch(() => res.status(500).send({ message: 'Нет пользователя с таким id' }));
 };
 
 const addUser = (req, res) => {
-  const { body } = req;
+  const { name, link } = req.body;
   User.countDocuments({})
-    .then((count) => User.create({ ...body, id: count }))
+    .then((count) => User.create({ id: count }, { name, link }, { new: true, runValidators: true }))
     .then((user) => res.send({ user }))
-    .catch(() => res.status(400).send({ message: 'Произошла ошибка при отправке данных' }));
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка при отправке данных' }));
 };
 
 const updateProfile = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(400).send({ message: 'Произошла ошибка при отправке данных' }));
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Введите корректные данные' });
+      }
+      res.send({ data: user });
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка при отправке данных' }));
 };
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(400).send({ message: 'Произошла ошибка при отправке данных' }));
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Введите корректные данные' });
+      }
+      res.send({ data: user });
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка при отправке данных' }));
 };
 
 module.exports = {
